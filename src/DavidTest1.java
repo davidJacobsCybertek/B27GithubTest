f5a2232449b4a8360184cfbbe59f7b428a4e9c2d
DavidJacobs <davidjacobs19782020@gmail.com>
2022-09-01 17:18:12 +0300

    changes more in David2

--- src/DavidTest1.java
+++ src/DavidTest1.java
@@ -1,6 +1,9 @@
 public class DavidTest1 {
 
     //here we have
+    //here we have
+    //here we have
+    //here we have
     public static void main(String[] args) {
         System.out.println("hi");
     }
